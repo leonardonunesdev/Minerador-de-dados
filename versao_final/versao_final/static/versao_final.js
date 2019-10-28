9643aaa040c3910ea0c046337d8ce7c6f847451a
Leonardo Nunes dos Santos <leonardonunes.255@hotmail.com>
2019-10-27 21:51:24 -0300

    Adicionado os gráficos comparativos

--- versao_final/versao_final/static/versao_final.js
+++ versao_final/versao_final/static/versao_final.js
@@ -16,6 +16,8 @@ $(function(){
     btnConsultar.on('click', function(){
         var febre = '';
         $(".loading-screen").show();
+        $(window).scrollTop(0);
+        $("html").css("overflow-y", "hidden");
 
         if(parseFloat(inpFebre.val()) > 37.5){
             febre = '37,5 +';
@@ -42,7 +44,7 @@ $(function(){
             success : function(data){
                 console.log("Apriori");
                 console.log(data);
-                geraHtmlResultados(data, divAprioriResultados);
+                geraHtmlResultados(data, divAprioriResultados, "Apriori");
 
                 $.ajax({
                     url     : 'regra_fp_growth',
@@ -61,8 +63,10 @@ $(function(){
                     success : function(data){
                         console.log("FP-Growth");
                         console.log(data);
-                        geraHtmlResultados(data, divFPGrowthResultados);
+                        geraHtmlResultados(data, divFPGrowthResultados, "FP-Growth");
                         $(".loading-screen").hide();
+                        $("html").css("overflow-y", "scroll");
+                        montaGraficos(); //Carrega os gráficos com as informações que vieram do back-end
                     }
                 });
             }
@@ -70,55 +74,102 @@ $(function(){
     })
 })
 
-function geraHtmlResultados(data, divResultados){
+//Variáveis para controlar os valores dos gráfico comparativo
+var numRegrasAprioriPositivas = 0;
+var numRegrasFpGrowthPositivas = 0;
+var numRegrasAprioriNegativas = 0;
+var numRegrasFpGrowthNegativas = 0;
+var numRegrasAprioriTotalGeradas = 0;
+var numRegrasFpGrowthTotalGeradas = 0;
+var numRegrasAprioriTotalAnalisadas = 0;
+var numRegrasFpGrowthTotalAnalisadas = 0;
+
+function geraHtmlResultados(data, divResultados, biblioteca){
     data = JSON.parse(data);
+    var headingOne = "";
+    var headingTwo = "";
+    var headingThree = "";
+    var collapseOne = "";
+    var collapseTwo = "";
+    var collapseThree = "";
+
+
+    if(biblioteca == "FP-Growth"){
+        headingOne = "headingFpGrowthOne";
+        headingTwo = "headingFpGrowthTwo";
+        headingThree = "headingFpGrowthThree";
+        collapseOne = "collapseFpGrowthOne";
+        collapseTwo = "collapseFpGrowthTwo";
+        collapseThree = "collapseFpGrowthThree";
+
+        //Popula variáveis do FP-Growth no gráfico
+        numRegrasFpGrowthPositivas = data.regrasPneumonia.length;
+        numRegrasFpGrowthNegativas = data.regrasNaoPneumonia.length;
+        numRegrasFpGrowthTotalAnalisadas = data.qtRegrasAnalisadas;
+        numRegrasFpGrowthTotalGeradas = data.qtRegrasGeradas;
+    }else{
+        headingOne = "headingAprioriOne";
+        headingTwo = "headingAprioriTwo";
+        headingThree = "headingAprioriThree";
+        collapseOne = "collapseAprioriOne";
+        collapseTwo = "collapseAprioriTwo";
+        collapseThree = "collapseAprioriThree";
+
+        //Popula variáveis do Apriori no gráfico
+        numRegrasAprioriPositivas = data.regrasPneumonia.length;
+        numRegrasAprioriNegativas = data.regrasNaoPneumonia.length;
+        numRegrasAprioriTotalAnalisadas = data.qtRegrasAnalisadas;
+        numRegrasAprioriTotalGeradas = data.qtRegrasGeradas;
+    }
 
     //Estrutura do html dos resultados Apriori e FP-Growth
+    //HTML do accordion
     var accordionRegrasHtml = " <div id='accordion'> \
                                     <div class='card'> \
-                                        <div class='card-header' id='headingAprioriOne' data-toggle='collapse' data-target='#collapseAprioriOne' aria-expanded='true' aria-controls='collapseAprioriOne'> \
+                                        <div class='card-header' id='" + headingOne + "'  data-toggle='collapse' data-target='#" + collapseOne + "' aria-expanded='false' aria-controls='" + collapseOne + "'> \
                                             <h5 class='mb-0'> \
-                                                <button class='btn btn-link' > \
+                                                <button class='btn btn-link collapsed'> \
                                                 Informações gerais \
                                                 </button> \
                                             </h5> \
                                         </div> \
-                                        <div id='collapseAprioriOne' class='collapse' aria-labelledby='headingAprioriOne' data-parent='#accordion'> \
+                                        <div id='" + collapseOne + "' class='collapse' aria-labelledby='" + headingOne + "' data-parent='#accordion'> \
                                             <div class='card-body first'> \
                                                 {0} \
                                             </div> \
                                         </div> \
                                     </div> \
                                     <div class='card'> \
-                                        <div class='card-header' id='headingAprioriTwo'  data-toggle='collapse' data-target='#collapseAprioriTwo' aria-expanded='false' aria-controls='collapseAprioriTwo'> \
+                                        <div class='card-header' id='" + headingTwo + "'  data-toggle='collapse' data-target='#" + collapseTwo + "' aria-expanded='false' aria-controls='" + collapseTwo + "'> \
                                             <h5 class='mb-0'> \
                                                 <button class='btn btn-link collapsed'> \
                                                 <p>Regras analisadas pneumonia positiva <span class='badge badge-positive'>{1}</span></p> \
                                                 </button> \
                                             </h5> \
                                         </div> \
-                                        <div id='collapseAprioriTwo' class='collapse' aria-labelledby='headingAprioriTwo' data-parent='#accordion'> \
-                                            <div class='card-body'> \
+                                        <div id='" + collapseTwo + "' class='collapse' aria-labelledby='" + headingTwo + "' data-parent='#accordion'> \
+                                            <div class='card-body card-table'> \
                                                 {2} \
                                             </div> \
                                         </div> \
                                     </div> \
                                     <div class='card'> \
-                                        <div class='card-header' id='headingAprioriThree' data-toggle='collapse' data-target='#collapseAprioriThree' aria-expanded='false' aria-controls='collapseAprioriThree'> \
+                                        <div class='card-header' id='" + headingThree + "' data-toggle='collapse' data-target='#" + collapseThree + "' aria-expanded='false' aria-controls='" + collapseThree + "'> \
                                             <h5 class='mb-0'> \
                                                 <button class='btn btn-link collapsed' > \
                                                 Regras analisadas pneumonia negativa <span class='badge badge-negative'>{3}</span> \
                                                 </button> \
                                             </h5> \
                                         </div> \
-                                        <div id='collapseAprioriThree' class='collapse' aria-labelledby='headingAprioriThree' data-parent='#accordion'> \
-                                            <div class='card-body'> \
+                                        <div id='" + collapseThree + "' class='collapse' aria-labelledby='" + headingThree + "' data-parent='#accordion'> \
+                                            <div class='card-body card-table'> \
                                                 {4} \
                                             </div> \
                                         </div> \
                                     </div> \
                                 </div>";
 
+    //HTML do accordion "Informações Gerais"
     var informacoesHtml = ' <p><strong>Nº de regras geradas: </strong>{0}</p>  \
                             <p><strong>Nº de regras analisadas: </strong>{1}</p> \
                             <p><strong>Nº regras Pneumonia positiva: </strong>{2}</p> \
@@ -126,7 +177,8 @@ function geraHtmlResultados(data, divResultados){
                             <p><strong>Chance de Pneumonia positiva: </strong>{4}</p> \
                             <p><strong>Chance de Pneumonia negativa: </strong>{5}</p>';
 
-    var regrasPneumoniaPositivaHtml = "<tablevid='tablePneumoniaPositiva' class='table table-bordered table-dark'> \
+    //HTML do accordion "Regras analisadas pneumonia positiva"
+    var regrasPneumoniaPositivaHtml = "<table id='tablePneumoniaPositiva' class='table table-bordered table-dark'> \
                                         <thead> \
                                             <tr> \
                                                 <th scope='col'>Suporte</th> \
@@ -145,6 +197,8 @@ function geraHtmlResultados(data, divResultados){
                                             {0} \
                                         </tbody> \
                                     </table>";
+    
+    //HTML do accordion "Regras analisadas pneumonia negativa"
     var regrasPneumoniaNegativaHtml = "<table id='tablePneumoniaNegativa' class='table table-bordered table-dark'> \
                                         <thead> \
                                             <tr> \
@@ -164,6 +218,8 @@ function geraHtmlResultados(data, divResultados){
                                             {0} \
                                         </tbody> \
                                     </table>";
+
+    //HTML das rows da tabela
     var tableRowHtml = "<tr> \
                             <td>{0}%</td> \
                             <td>{1}</td> \
@@ -176,8 +232,8 @@ function geraHtmlResultados(data, divResultados){
                             <td>{8}</td> \
                             <td>{9}</td> \
                         </tr>";
-    var tableRowsPneumonia = "";
-    var tableRowsNaoPneumonia= "";
+    var tableRowsPneumonia = ""; //HTML das rows do accordion "Regras analisadas pneumonia positiva"
+    var tableRowsNaoPneumonia= ""; //HTML das rows do accordion "Regras analisadas pneumonia negativa"
     
     //Começo montagem html das informações gerais do accordion
 
@@ -210,7 +266,6 @@ function geraHtmlResultados(data, divResultados){
                                             .replace('{7}', data.regrasPneumonia[numRegra].suor == "" ? "-" : data.regrasPneumonia[numRegra].suor)
                                             .replace('{8}', data.regrasPneumonia[numRegra].nausea == "" ? "-" : data.regrasPneumonia[numRegra].nausea)
                                             .replace('{9}', data.regrasPneumonia[numRegra].pneumonia == "" ? "-" : data.regrasPneumonia[numRegra].pneumonia);
-                                                
     }
 
     regrasPneumoniaPositivaHtml = regrasPneumoniaPositivaHtml.replace('{0}', tableRowsPneumonia);
@@ -250,4 +305,57 @@ function geraHtmlResultados(data, divResultados){
 
     divResultados.html(accordionRegrasHtml);
     //Fim montagem html completo
+
+
+
+}
+
+function montaGraficos(){
+    new Chart(document.getElementById("graficoRegrasAnalisadas"), {
+        type: 'bar',
+        data: {
+          labels: ["Pneumonia positiva", "Pneumonia negativa"],
+          datasets: [
+            {
+              label: "Apriori",
+              backgroundColor: "lightgray",
+              data: [numRegrasAprioriPositivas, numRegrasAprioriNegativas]
+            }, {
+              label: "FP-Growth",
+              backgroundColor: "#777",
+              data: [numRegrasFpGrowthPositivas, numRegrasFpGrowthNegativas]
+            }
+          ]
+        },
+        options: {
+          title: {
+            display: true,
+            text: 'Regras analisadas (Apriori e FP-Growth)'
+          }
+        }
+    });
+
+    new Chart(document.getElementById("graficoRegrasGeradas"), {
+        type: 'bar',
+        data: {
+          labels: ["Total regras geradas", "Total regras analisadas"],
+          datasets: [
+            {
+              label: "Apriori",
+              backgroundColor: "lightgray",
+              data: [numRegrasAprioriTotalGeradas, numRegrasAprioriTotalAnalisadas]
+            }, {
+              label: "FP-Growth",
+              backgroundColor: "#777",
+              data: [numRegrasFpGrowthTotalGeradas, numRegrasFpGrowthTotalAnalisadas]
+            }
+          ]
+        },
+        options: {
+          title: {
+            display: true,
+            text: 'Regras gerasdas vs Regras analisadas'
+          }
+        }
+    });
 }
